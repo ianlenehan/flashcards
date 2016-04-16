70f20a23aed139ea3b05e0285995deb0e3c5ec88
Emily Quill <emilyquill@outlook.com>
2016-04-16 11:17:32 +1000

    adding basic backbone scaffold

---
+++ app/assets/javascripts/views/AppView.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+  el: "#main",
+
+  render: function() {
+    var appViewTemplate = $('#appViewTemplate').html();
+    // TODO: Setup appViewTemlplate
+
+  }
+});
