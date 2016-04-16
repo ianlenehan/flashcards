d0539eda47d6eb72c8577b84c0506d116a01931a
twosmalltrees <harrison@twosmalltrees.com>
2016-04-16 11:14:22 +1000

    Seedy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#### ------------- CREATING USERS ------------ ####
 
 User.destroy_all
 
@@ -13,6 +7,10 @@ emily = User.create(name_first: 'Emily', name_last: 'Quill', email: 'emily@examp
 ian = User.create(name_first: 'Ian', name_last: 'Lenehan', email: 'ian@example.com', password: 'chicken', password_confirmation: 'chicken', lifetime_score: 0, admin: false, photo: 'http://www.fillmurray.com/150/150')
 harrison = User.create(name_first: 'Harrison', name_last: 'Reid', email: 'harrison@example.com', password: 'chicken', password_confirmation: 'chicken', lifetime_score: 0, admin: false, photo: 'http://www.fillmurray.com/150/150')
 jackAdmin = User.create(name_first: 'Jack', name_last: 'Jeffress', email: 'admin@example.com', password: 'chicken', password_confirmation: 'chicken', lifetime_score: 0, admin: true, photo: 'http://www.fillmurray.com/150/150')
+
+
+
+#### ------------- CREATING CATEGORIES ------------ ####
 
 Category.destroy_all
 
@@ -24,33 +22,56 @@ biology = Category.create(name: 'Biology')
 us_civil_war = Category.create(name: 'US Civil War')
 
 
+#### ------------- CREATING DECKS ------------ ####
+
 Deck.destroy_all
 
-spanish101 = Deck.create(name: 'Spanish 101')
-spanishFood = Deck.create(name: 'Spanish - Food')
-spanishGettingAround = Deck.create(name: 'Spanish - Getting Around')
+# SPANISH
 
-french101 = Deck.create(name: 'French 101')
-frenchTravel = Deck.create(name: 'French - Travel')
-frenchRomance = Deck.create(name: 'French - Romance')
+spanish_101 = Deck.create(name: 'Spanish 101')
+spanish_food = Deck.create(name: 'Spanish - Food')
+spanish_getting_around = Deck.create(name: 'Spanish - Getting Around')
 
-jsVariables = Deck.create(name: 'JS Variables')
-jsConditionals = Deck.create(name: 'JS Conditionals')
+# FRENCH
 
-rubyLoops = Deck.create(name: 'Ruby Loops')
-rubyClasses = Deck.create(name: 'Ruby Classes')
+french_101 = Deck.create(name: 'French 101')
+french_travel = Deck.create(name: 'French - Travel')
+french_dating = Deck.create(name: 'French - Dating')
 
-biology = Deck.create(name: 'Basic Evolutionary Theory')
-biology = Deck.create(name: 'Advanced Genetics')
+# BIOLOGY
 
-civilWar = Deck.create(name: '')
+evolution = Deck.create(name: 'Basic Evolutionary Theory')
+genetics = Deck.create(name: 'Advanced Genetics')
+
+# JAVASCRIPT
+
+js_variables = Deck.create(name: 'JS Variables')
+js_conditionals = Deck.create(name: 'JS Conditionals')
+
+# RUBY
+
+ruby_loops = Deck.create(name: 'Ruby Loops')
+ruby_classes = Deck.create(name: 'Ruby Classes')
+
+# US CIVIL WAR
+
+custers_last_stand = Deck.create(name: 'Custers Last Stand')
+general_grant = Deck.create(name: 'General Grant')
+general_lee = Deck.create(name: 'General Lee')
 
 
+#### ------------- ASSIGNING DECKS TO CATEGORIES ------------ ####
+
+spanish.decks << spanish_101 << spanish_food << spanish_getting_around
+french.decks << french_101 << french_travel << french_dating
+biology.decks << evolution << genetics
+javascript.decks << js_variables << js_conditionals
+ruby.decks << ruby_loops << ruby_classes
+us_civil_war.decks << custers_last_stand << general_grant << general_lee
 
 
-create_table "decks", force: :cascade do |t|
-  t.string   "name"
-  t.integer  "user_id"
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-end
+#### ------------- ASSIGNING DECKS TO USERS ------------ ####
+
+emily.decks << spanish_101 << spanish_food << spanish_getting_around
+emily.decks << french_101 << french_travel << french_dating
+emily.decks << evolution << genetics
