c4811cdf0ab1bcac44c4ec7d1b6b0b13a54cf814
Emily Quill <emilyquill@outlook.com>
2016-04-17 12:19:07 +1000

    Each deck displaying only the cards belongong to it

--- app/assets/javascripts/views/DeckView.js
+++ app/assets/javascripts/views/DeckView.js
@@ -4,11 +4,12 @@ app.DeckView = Backbone.View.extend({
   el: '#deck',
 
   render: function() {
+    // debugger;
     $('#deckList').remove();
     $('#cards').append('<h2>'+this.model.attributes.name+'</h2>');
 
-    _.each(this.collection.models, function (card) {
-      var cardObject = card.attributes;
+    _.each(this.model.attributes.cards, function (card) {
+      var cardObject = card;
       var cardTemplate = $('#cardTemplate').html();
       var cardHTML = _.template( cardTemplate );
       $('#cards').append( cardHTML( cardObject ) );
