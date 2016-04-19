07db2328ef27786da447f17ff61f3ffacdeef6a5
Ian Lenehan <ianlenehan@gmail.com>
2016-04-19 18:11:43 +1000

    updated style effects

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,11 +6,4 @@ $(document).ready(function() {
   app.currentUser = new app.CurrentUser();
   Backbone.history.start();
 
-  $('#admin-button').on('mouseover', function () {
-    $('#admin-links').fadeIn();
-  });
-
-  $('#admin-links').on('click', function () {
-    $('#admin-links').fadeOut();
-  });
 });
