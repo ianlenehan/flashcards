264f77f14a6baed205e7d70f031f4dfbf6e328d1
Emily Quill <emilyquill@outlook.com>
2016-04-21 09:38:06 +1000

    Authorise working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :fetch_user
+  before_action :authorise, :except => [:new, :create, :home]
 
   private
   def fetch_user
     @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
     session[:user_id] = nil unless @current_user.present?
+  end
+
+  def authorise
+    redirect_to root_path unless (@current_user.present?)
   end
 end
