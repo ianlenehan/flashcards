c6fb9a175680d2dbd5a5822d48b203f5bb968b4e
Ian Lenehan <ianlenehan@gmail.com>
2016-04-21 22:05:49 +1000

    sorted categories and added further styling to rails side

--- app/assets/javascripts/collections/Categories.js
+++ app/assets/javascripts/collections/Categories.js
@@ -4,8 +4,12 @@ app.Categories = Backbone.Collection.extend({
   url: '/categories',
   model: app.Category,
 
+  comparator: function(m) {
+    return m.get('name');
+  },
+
   initialize: function () {
-  
+
   }
 
 });
