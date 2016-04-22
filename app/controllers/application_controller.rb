0b38667f16cce40af9b1bb4806b6f71a59d6041a
Emily Quill <emilyquill@outlook.com>
2016-04-22 10:47:52 +1000

    No access to home page unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :fetch_user
-  before_action :authorise, :except => [:new, :create, :home]
+  before_action :authorise, :except => [:new, :create]
 
   private
   def fetch_user
